1d56ac81d9dd2ff31a3f5cb76f5fdfcaba8a256e
Laura DeLuca <143122298+laurasdeluca@users.noreply.github.com>
2026-09-22 13:27:40 -0400

    Bump Field Log cache

--- fitness-dashboard/web/sw.js
+++ fitness-dashboard/web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "field-log-v7";
+const CACHE = "field-log-v9";
 const SHELL = ["./", "./index.html", "./style.css", "./app.js", "./config.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
