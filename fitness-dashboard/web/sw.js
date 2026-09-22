fe38f513d1ad2947e98e19ac93743ef15a1d7011
Laura DeLuca <143122298+laurasdeluca@users.noreply.github.com>
2026-09-22 13:15:56 -0400

    Refresh dashboard cache for recovery update

--- fitness-dashboard/web/sw.js
+++ fitness-dashboard/web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "field-log-v3";
+const CACHE = "field-log-v4";
 const SHELL = ["./", "./index.html", "./style.css", "./app.js", "./config.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
